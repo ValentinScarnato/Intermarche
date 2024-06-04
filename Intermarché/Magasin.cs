9cbcf58414ccef37a1458e2ac0978f9506839ada
romerin <nathan.romeri@etu.univ-smb.fr>
2024-06-04 09:52:14 +0200

    modif classe + contrainte longueur

---
+++ Intermarché/Magasin.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intermarché
+{
+    public class Magasin
+    {
+		private int num_vehicule;
+
+		public int Num_vehicule
+        {
+			get { return num_vehicule; }
+			set { num_vehicule = value; }
+		}
+
+		private string nom_vehicule;
+
+		public string Nom_vehicule
+        {
+			get { return nom_vehicule; }
+			set { nom_vehicule = value; }
+		}
+
+        private string adresse_rue_magasin;
+
+        public string Adresse_rue_magasin
+        {
+            get { return adresse_rue_magasin; }
+            set
+            {
+                if (value.Length > 200)
+                    throw new ArgumentException("La rue de l'adresse du magasin est trop longue.");
+                this.adresse_rue_magasin = value;
+            }
+        }
+
+        private string adresse_cp_magasin;
+
+        public string Adresse_cp_magasin
+        {
+            get { return adresse_cp_magasin; }
+            set
+            {
+                if (value.Length > 5)
+                    throw new ArgumentException("Le code postal de l'adresse du magasin est trop long.");
+                this.adresse_cp_magasin = value;
+            }
+        }
+
+        private string adresse_ville_magasin;
+
+        public string Adresse_ville_magasin
+        {
+            get { return adresse_ville_magasin; }
+            set
+            {
+                if (value.Length > 50)
+                    throw new ArgumentException("La ville de l'adresse du magasin est trop longue.");
+                this.adresse_ville_magasin = value;
+            }
+        }
+
+        private string horraire_magasin;
+
+        public string Horraire_magasin
+        {
+            get { return horraire_magasin; }
+            set {
+                if (value.Length > 20)
+                    throw new ArgumentException("L'horraire du magasin à une valeur trop longue.");
+                this.horraire_magasin = value;
+            }
+        }
+    }
+}
---
+++ Intermarché/Reservation_table.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intermarché
+{
+    class Reservation_table
+    {
+		private int num_reservation;
+
+		public int Num_reservation
+        {
+			get { return num_reservation; }
+			set { num_reservation = value; }
+		}
+
+		private int num_assurance;
+
+		public int Num_assurance
+        {
+			get { return num_assurance; }
+			set { num_assurance = value; }
+		}
+
+        private int num_client;
+
+        public int Num_client
+        {
+            get { return num_client; }
+            set { num_client = value; }
+        }
+
+		private DateTime date_reservation;
+
+		public DateTime Date_reservation
+        {
+			get { return date_reservation; }
+			set { date_reservation = value; }
+		}
+
+        private DateTime date_debut_reservation;
+
+        public DateTime Date_debut_reservation
+        {
+            get { return date_debut_reservation; }
+            set { date_debut_reservation = value; }
+        }
+
+        private DateTime date_fin_reservation;
+
+        public DateTime Date_fin_reservation
+        {
+            get { return date_fin_reservation; }
+            set { date_fin_reservation = value; }
+        }
+
+        private decimal montant_reservation;
+
+        public decimal Montant_reservation
+        {
+            get { return montant_reservation; }
+            set { montant_reservation = value; }
+        }
+
+        private string forfait_km;
+
+        public string Forfait_km
+        {
+            get { return forfait_km; }
+            set {
+                if (value.Length > 10)
+                    throw new ArgumentException("Le forfait kilométirque à une valeur trop longue");
+            }
+        }
+
+    }
+}
