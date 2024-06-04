0b9856ef1750b2459f1e7bbfc00b7b3afe386f9f
ValentinScarnato <valentin.scarnato@etu.univ-savoie.fr>
2024-06-04 09:52:50 +0200

    _ç

---
+++ Intermarché/ApplicationData.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intermarché
+{
+    public class ApplicationData
+    {
+
+        private ObservableCollection<Client> lesClients;
+        private SqlConnection connexion = null;   // futur lien à la BD
+
+
+        public ObservableCollection<Client> LesClients
+        {
+            get
+            {
+                return this.lesClients;
+            }
+
+            set
+            {
+                this.lesClients = value;
+            }
+        }
+
+        public SqlConnection Connexion
+        {
+            get
+            {
+                return this.connexion;
+            }
+
+            set
+            {
+                this.connexion = value;
+            }
+        }
+
+        public ApplicationData()
+        {
+            this.ConnexionBD();
+            this.Read();
+            try
+            {
+                Connexion = new SqlConnection();
+                Connexion.ConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=P:\S2\R2.02_IHM_WPF\TD3_Binding_BD\P1_BD_Locale\BD_Clients.mdf;Integrated Security=True";
+                // à compléter dans les "" 
+                // @ sert à enlever tout pb avec les caractères 
+                Connexion.Open();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("pb de connexion : " + e);
+                // juste pour le debug : à transformer en MsgBox 
+            }
+        }
+        public void ConnexionBD()
+        {
+
+        }
+        public int Read()
+        {
+            String sql = "SELECT id, nom,prenom,email,genre,telephone, dateNaissance FROM Client";
+            try
+            {
+                SqlDataAdapter dataAdapter = new SqlDataAdapter(sql, Connexion);
+                DataTable dataTable = new DataTable();
+                dataAdapter.Fill(dataTable);
+                foreach (DataRow res in dataTable.Rows)
+                {
+                    Client nouveau = new Client(int.Parse(res["id"].ToString()),
+                    res["nom"].ToString(), res["prenom"].ToString(),
+                    res["email"].ToString(), DateTime.Parse(res["dateNaissance"].ToString()),
+                    res["telephone"].ToString(),
+                    (GenreClient)char.Parse(res["genre"].ToString()));
+                    LesClients.Add(nouveau);
+                }
+                return dataTable.Rows.Count;
+            }
+            catch (SqlException e)
+            { Console.WriteLine("pb de requete : " + e); return 0; }
+        }
+        public int Create(Client c)
+        {
+            String sql = $"insert into client (nom,prenom,email,genre,telephone, dateNaissance)"
+            + $" values ('{c.Nom}','{c.Prenom}','{c.Email}'"
+            + $",'{(char)c.Genre}','{c.Telephone}', "
+            + $"'{c.DateNaissance.Year}-{c.DateNaissance.Month}-{c.DateNaissance.Day}'); ";
+            try
+            {
+                SqlCommand cmd = new SqlCommand(sql, Connexion);
+                int nb = cmd.ExecuteNonQuery();
+                return nb;
+                //nb permet de connaître le nb de lignes affectées par un insert, update, delete
+            }
+            catch (Exception sqlE)
+            {
+                Console.WriteLine("pb de requete : " + sql + "" + sqlE);
+                // juste pour le debug : à transformer en MsgBox 
+                return 0;
+            }
+        }
+        public int Delete(int idClient)
+        {
+            String sql = "delete from client where idClient = @idClient;";
+            try
+            {
+                SqlCommand cmd = new SqlCommand(sql, Connexion);
+                cmd.Parameters.AddWithValue("@idClient", idClient);
+                int nb = cmd.ExecuteNonQuery();
+                return nb;
+            }
+            catch (Exception sqlE)
+            {
+                Console.WriteLine("Erreur de requête : " + sql + " " + sqlE);
+                return 0;
+            }
+        }
+        public int Update(Client c)
+        {
+            String sql = $"insert into client (nom,prenom,email,genre,telephone, dateNaissance)"
+            + $" values ('{c.Nom}','{c.Prenom}','{c.Email}'"
+            + $",'{(char)c.Genre}','{c.Telephone}', "
+            + $"'{c.DateNaissance.Year}-{c.DateNaissance.Month}-{c.DateNaissance.Day}'); ";
+            try
+            {
+                SqlCommand cmd = new SqlCommand(sql, Connexion);
+                int nb = cmd.ExecuteNonQuery();
+                return nb;
+                //nb permet de connaître le nb de lignes affectées par un insert, update, delete
+            }
+            catch (Exception sqlE)
+            {
+                Console.WriteLine("pb de requete : " + sql + "" + sqlE);
+                // juste pour le debug : à transformer en MsgBox 
+                return 0;
+            }
+        }
+
+
+    }
+}
--- Intermarché/Client.cs
+++ Intermarché/Client.cs
@@ -1,13 +1,155 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Intermarché
 {
-    class Client
+    public enum GenreClient { Neutre = 'N', Homme = 'H', Femme = 'F' };
+    public class Client : ICloneable
     {
-        
+
+        private GenreClient genre;
+        private string nom;
+        private string prenom;
+        private string email;
+        private DateTime dateNaissance;
+        private string telephone;
+        public Client()
+        {
+            this.dateNaissance = DateTime.Today;
+        }
+
+        public Client(int id, string nom, string prenom, string email)
+        {
+            this.Nom = nom;
+            this.Prenom = prenom;
+            this.Email = email;
+            this.Id = id;
+        }
+
+        public Client(int id, string nom, string prenom, string email, DateTime dateNaissance, string telephone) : this(id, nom, prenom, email)
+        {
+            this.DateNaissance = dateNaissance;
+            this.Telephone = telephone;
+        }
+
+        public Client(int id, string nom, string prenom, string email, DateTime dateNaissance, string telephone, GenreClient genre) : this(id, nom, prenom, email, dateNaissance, telephone)
+        {
+            this.Genre = genre;
+        }
+        private int id;
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public string Nom
+        {
+            get
+            {
+                return this.nom;
+            }
+
+            set
+            {
+                if (String.IsNullOrEmpty(value))
+                    throw new ArgumentException("Le nom doit être renseigné");
+                this.nom = value;
+            }
+        }
+
+        public string Prenom
+        {
+            get
+            {
+                return this.prenom;
+            }
+
+            set
+            {
+                if (String.IsNullOrEmpty(value))
+                    throw new ArgumentException("Le prénom doit être renseigné");
+                this.prenom = value;
+            }
+        }
+
+        public string Email
+        {
+            get
+            {
+                return this.email;
+            }
+
+            set
+            {
+                try
+                {
+                    MailAddress mail = new MailAddress(value);
+                    this.email = value;
+                }
+                catch (Exception e) { throw new ArgumentException("L'email est invalide."); }
+            }
+        }
+
+        public DateTime DateNaissance
+        {
+            get
+            {
+                return this.dateNaissance;
+            }
+
+            set
+            {
+                if (value == DateTime.Today)
+                    throw new ArgumentException("La date ne peut être aujourd'hui.");
+
+                this.dateNaissance = value;
+            }
+        }
+
+        public string Telephone
+        {
+            get
+            {
+                return this.telephone;
+            }
+
+            set
+            {
+                if (!Regex.IsMatch(value, "^[0-9]{10}$"))
+                    throw new ArgumentException("Le format du tel n'est pas correct. 10 chiffres attendus.");
+
+                this.telephone = value;
+            }
+        }
+
+        public GenreClient Genre
+        {
+            get
+            {
+                return this.genre;
+            }
+
+            set
+            {
+                this.genre = value;
+            }
+        }
+
+        public object Clone()
+        {
+            return new Client(this.id, this.nom, this.prenom, this.email, this.DateNaissance, this.telephone, this.genre);
+        }
+
+        public override string? ToString()
+        {
+            return Nom + " " + Prenom + " " + Email;
+        }
     }
 }
