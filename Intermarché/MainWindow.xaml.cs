62a7517c684712fbeffeb221d69f9e4c4ef80c34
ValentinScarnato <valentin.scarnato@etu.univ-savoie.fr>
2024-06-05 13:36:54 +0200

    cleaning

--- Intermarché/MainWindow.xaml.cs
+++ Intermarché/MainWindow.xaml.cs
@@ -28,52 +28,8 @@ namespace Intermarché
             InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            FentreAOuvrir = "Connexion";
-            OuvertureFenetre();
-        }
-
-        public void OuvertureFenetre()
-        {
-            resa = false;
-            while (!quitter && !resa)
-            {
-                switch (fentreAOuvrir)
-                {
-                    case "Connexion":
-                        {
-                            Connexion connexion = new Connexion();
-                            connexion.ShowDialog();
-                            break;
-                        }
-                    case "Résa":
-                        {
-                            resa = true;
-                            break;
-                        }
-                    case "Quitter":
-                        {
-                            quitter = true;
-                            break;
-                        }
-
-                }
-            }
-
-            Console.WriteLine(fentreAOuvrir + " est ouvert");
-
-            if (quitter)
-            {
-                Console.WriteLine(fentreAOuvrir + "pour quitter application");
-                Application.Current.Shutdown();
-            }
-        }
-
-        
-=======
->>>>>>> 638c2b0a32afe9cdfbbef85e68be909b9bf270b2
-        private bool VerifierLogin(string login, string mdp)
+       
+        private bool VerifierLogin()
         {
             bool isValid = false;
 
@@ -81,28 +37,6 @@ namespace Intermarché
             string connectionString = "Server=srv-peda-new;" + "port=5433;" +
                 "Database=votreBase;" + "Search Path = votreSchemaPostGresql;" + "uid=votreLogin;" +
                 "password=votrePassword;";
-
-            using (NpgsqlConnection connection = new NpgsqlConnection(connectionString))
-            {
-                string query = "SELECT COUNT(1) FROM Employe WHERE Login = @Login AND Mdp = @Mdp";
-
-                using (NpgsqlCommand command = new NpgsqlCommand(query, connection))
-                {
-                    command.Parameters.AddWithValue("@Login", login);
-                    command.Parameters.AddWithValue("@Mdp", mdp);
-
-                    connection.Open();
-
-                    int count = (int)(command.ExecuteScalar());
-
-                    if (count == 1)
-                    {
-                        isValid = true;
-                    }
-
-                    connection.Close();
-                }
-            }
 
             return isValid;
         }
