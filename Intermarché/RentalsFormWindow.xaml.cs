752c546f5cf8ad725c4df9474c0659498a208437
ValentinScarnato <155959182+ValentinScarnato@users.noreply.github.com>
2024-06-10 11:27:44 +0200

    Ajouts divers et variés comme datagrid les clients, ajoutClients et refresh, Fix réservation, RentalForm

--- Intermarché/RentalsFormWindow.xaml.cs
+++ Intermarché/RentalsFormWindow.xaml.cs
@@ -1,8 +1,10 @@
 ﻿using Intermarché.Classes;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Windows;
+using System.Windows.Data;
 
 namespace Intermarché
 {
@@ -13,14 +15,37 @@ namespace Intermarché
         public RentalsFormWindow()
         {
             InitializeComponent();
-        }
+            Refresh();
+            //dgLesClients.Items.Filter = FiltreClient;
 
+        }
+        private void Refresh()
+        {
+            if (!this.IsInitialized) return;
+            ((ICollectionView)this.dgLesClients.ItemsSource).Refresh();
+        }
         private void butValiderResa_Click(object sender, RoutedEventArgs e)
         {
-            int numAssurance;
+            int numAssurance=0;
             int numClient;
             string forfaitKm = "0";
             int nbJours;
+            double PourcentageAssurance =0;
+            if (rbAssurance1.IsChecked == true)
+            {
+                numAssurance = 1;
+                PourcentageAssurance = 0.05;
+            }
+            else if (rbAssurance2.IsChecked == true)
+            {
+                numAssurance = 2;
+                PourcentageAssurance = 0.07;
+            }
+            else if (rbAssurance3.IsChecked == true)
+            {
+                numAssurance = 3;
+                PourcentageAssurance = 0.1;
+            }
             DateTime dateResa = DateTime.Today;
             DateTime? dateDebutResa = dpDateDebutReservation.SelectedDate;
             DateTime? dateFinResa = dpDateFinReservation.SelectedDate;
@@ -28,9 +53,9 @@ namespace Intermarché
             DateTime dateDebut = (DateTime)dateDebutResa.Value;
             DateTime dateFin = (DateTime)dateFinResa.Value;
             nbJours = (dateFin - dateDebut).Days;
-            if (!int.TryParse(tbNumAssurance.Text, out numAssurance) || !int.TryParse(tbNumCLient.Text, out numClient))
+            if (!int.TryParse(tbNumCLient.Text, out numClient))
             {
-                MessageBox.Show("Veuillez entrer des numéros valides pour l'assurance et le client.");
+                MessageBox.Show("Veuillez entrer des numéros valides pour le client.");
                 return;
             }
 
@@ -43,6 +68,7 @@ namespace Intermarché
                 if (VehiculesSelectionnés != null && VehiculesSelectionnés.Any())
                 {
                     double montant = (nbJours+1) * (VehiculesSelectionnés.Sum(v => v.PrixLocation));
+                    montant += Math.Round(PourcentageAssurance * montant,0); 
 
                     Reservation_table resa = new Reservation_table(
                         int.Parse(numReservation),
@@ -57,8 +83,7 @@ namespace Intermarché
 
                     ((ApplicationData)this.DataContext).LesReservations.Add(resa);
                     resa.Create();
-                    MessageBox.Show("Réservation ajoutée avec succès !");
-                    MessageBox.Show($"Le montant total de la réservation est de {montant} euros");
+                    MessageBox.Show($"Réservation ajoutée avec succès !\nLe montant total de la réservation est de {montant} euros");
                     
                 }
             }
@@ -68,5 +93,19 @@ namespace Intermarché
             }
             
         }
+        private bool FiltreClient(object obj)
+        {
+            Client unClient = obj as Client;
+            if (String.IsNullOrEmpty(tbRechercherClient.Text))
+                return true;
+            else
+                return (unClient.NumClient.ToString().StartsWith(tbRechercherClient.Text, StringComparison.OrdinalIgnoreCase) || unClient.NomClient.ToString().StartsWith(tbRechercherClient.Text, StringComparison.OrdinalIgnoreCase));
+        }
+        /*
+
+        private void tbRechercherClient_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            CollectionViewSource.GetDefaultView(dgLesClients.ItemsSource).Refresh();
+        }*/
     }
 }
